e762658143bede11c034192da76909b47c963829
vitaliy <vitalij.leontjev@tetrika.school>
2024-08-29 15:10:46 +0300

    completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js',
